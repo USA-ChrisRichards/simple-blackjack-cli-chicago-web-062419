65d9c0562acde97c042148b64cacf2ef53bb3a80
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 16:33:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,11 +30,11 @@ end
 
 def hit?(number)
   prompt_user
-  get_user_input
-  if get_user_input == 'h'
+  user_input = get_user_input
+  if user_input == 'h'
     deal_card
     (card_total = deal_card)
-  elsif get_user_input == 's'
+  elsif user_input == 's'
   display_card_total
   else
     invalid_command
